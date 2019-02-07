d126138ef0087934a5013b8aacfd1609a1a44f60
sharkk <sharkk@list.ru>
2019-02-07 09:17:21 +0300

    Fix go.sh

--- scrapers/go.sh
+++ scrapers/go.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /home/iliya/stroy-parser35/scrapers
 . "../venv/bin/activate"
-cd /home/iliya/stroy-parser35/scrapers
 wait
 python apline.py >> apline.log &
 python bober.py >> bober.log &
