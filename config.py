15d25f27a158bbaf5ac03eb1dc136d3df6c1bf91
sharkk <sharkk@list.ru>
2019-02-07 11:26:15 +0300

    Fix

--- config.py
+++
@@ -1,5 +0,0 @@
-class Config(object):
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://iliya:My_SQL_Frekf87@localhost/sp35'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = '\xa2Jt\xb5\x18\x90\xf0u\x8e\x00VV\xf2\xff\xb6\xe5\x06\x1a\xabQ\x1c\x1a\xec\x0c'
