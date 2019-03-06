38639f6fb2618fd21955a5a1032e8ca621b724da
Sharkk-Home <Sharkk@list.ru>
2019-03-06 09:02:51 +0300

    Fix rm_ols_files

--- scrapers/shop_csv/rm_old_files.py
+++ scrapers/shop_csv/rm_old_files.py
@@ -1,15 +1,17 @@
-import glob, os
+import glob, os, sys
 
 from datetime import datetime, timedelta
 
 
 all_files = glob.glob('*.csv')
 
+rm_date = int(sys.argv[1])
+
 delete_files = []
 for file in all_files:
     date_file = file.split('_')[0]
     date_file = datetime.strptime(date_file, '%d%m%Y')
-    if datetime.now() - date_file > timedelta(days=4):
+    if datetime.now() - date_file > timedelta(days=rm_date):
         delete_files.append(file)
 
 if not delete_files:
